54bf4806d79e8841b2936bfbea47d45c30ea00ce
dineshkummarc <dineshkummarc@gmail.com>
2013-02-07 17:26:44 +0530

    moved files
    moved files to /js, /images and /css

---
+++ js/UpdateClock.js
@@ -0,0 +1,4 @@
+<reference path="jquery-1.4.2.min.js" />
+
+function updateClock ( ) {   var currentTime = new Date ( );    var currentHours = currentTime.getHours ( );  var currentMinutes = currentTime.getMinutes ( ); var currentSeconds = currentTime.getSeconds ( ); currentMinutes = ( currentMinutes < 10 ? "0" : "" ) + currentMinutes; currentSeconds = ( currentSeconds < 10 ? "0" : "" ) + currentSeconds;  var timeOfDay = ( currentHours < 12 ) ? "AM" : "PM";  currentHours = ( currentHours > 12 ) ? currentHours - 12 : currentHours; currentHours = ( currentHours == 0 ) ? 12 : currentHours; var currentTimeString = currentHours + ":" + currentMinutes + ":" + currentSeconds + " " + timeOfDay; $("#TaskBarTime").html(currentTimeString); } 
+$(document).ready(function() {  setInterval('updateClock()', 1000); }); 
